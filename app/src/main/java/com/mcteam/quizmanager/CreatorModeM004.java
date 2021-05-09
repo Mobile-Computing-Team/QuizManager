151ebeb264d90c0b2c49e3e8d9e57976af1ffdb3
mehroz-muzaffar-SEF18 <79619614+mehroz-muzaffar-SEF18@users.noreply.github.com>
2021-05-09 17:29:17 +0500

    Added 2 more OnClick Events

---
+++ app/src/main/java/com/mcteam/quizmanager/CreatorModeM004.java
@@ -0,0 +1,14 @@
+package com.mcteam.quizmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatorModeM004 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creator_mode_m004);
+    }
+}
