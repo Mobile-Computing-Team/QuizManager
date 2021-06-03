b66739f6b0e125bdb14b274032a67c6ef5b414da
mehroz-muzaffar-SEF18 <79619614+mehroz-muzaffar-SEF18@users.noreply.github.com>
2021-06-03 15:33:35 +0500

    MainActivity Creator Mode Password Update M004

--- app/src/main/java/com/mcteam/quizmanager/MainActivity.java
+++ app/src/main/java/com/mcteam/quizmanager/MainActivity.java
@@ -2,19 +2,28 @@ package com.mcteam.quizmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DBHelper db=new DBHelper(this);
+        db=new DBHelper(this);
         SQLiteDatabase database=db.getWritableDatabase();
         setContentView(R.layout.activity_main);
     }
@@ -32,8 +41,81 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void GotoCreatorMode(View view) {
-        Intent intent=new Intent(this,CreatorModeM004.class);
-        startActivity(intent);
+        final Intent intent=new Intent(this,CreatorModeM004.class);
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        LayoutInflater inflater=LayoutInflater.from(this);
+        final View dialogView;
+        final EditText dialogEditText;
+        if(db.isPasswordTableEmpty()) {
+            dialogView = inflater.inflate(R.layout.change_title_dialog, null);
+            dialogEditText = dialogView.findViewById(R.id.sectionTitle);
+            dialogEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            dialogEditText.setHint("Enter Password");
+            TextView inputLabel = dialogView.findViewById(R.id.input_label);
+            inputLabel.setText("Password: ");
+            final AlertDialog dialog = builder.setView(dialogView).setPositiveButton("Create", null).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                }
+            }).setTitle("Please Enter Password You want to use for this Mode").create();
+            dialog.show();
+            Button positiveButton = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+            positiveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String newPassword = dialogEditText.getText().toString();
+                    if(newPassword.isEmpty())
+                    {
+                        ((TextView)dialogView.findViewById(R.id.error)).setVisibility(View.VISIBLE);
+                        ((TextView)dialogView.findViewById(R.id.error)).setText("Password Can't be Empty!");
+                    }
+                    else if(newPassword.length()<8 || newPassword.length()>=15)
+                    {
+                        ((TextView)dialogView.findViewById(R.id.error)).setVisibility(View.VISIBLE);
+                        ((TextView)dialogView.findViewById(R.id.error)).setText("Password Length must be Between 8 and 15!");
+                    }
+                    else
+                    {
+                        db.addPassword(newPassword);
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Password Created Successfully! Now try entering your creator mode using this password", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+        else
+        {
+            dialogView = inflater.inflate(R.layout.change_title_dialog, null);
+            dialogEditText = dialogView.findViewById(R.id.sectionTitle);
+            dialogEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            dialogEditText.setHint("Enter Password");
+            TextView inputLabel = dialogView.findViewById(R.id.input_label);
+            inputLabel.setText("Password: ");
+            final AlertDialog dialog = builder.setView(dialogView).setPositiveButton("Submit", null).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                }
+            }).setTitle("Enter Password You want to use for this Mode").create();
+            dialog.show();
+            Button positiveButton = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+            positiveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String newPassword = dialogEditText.getText().toString();
+                    if(db.isCreator(newPassword))
+                    {
+                        Toast.makeText(MainActivity.this, "Creator Mode Access Granted!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        ((TextView)dialogView.findViewById(R.id.error)).setVisibility(View.VISIBLE);
+                        ((TextView)dialogView.findViewById(R.id.error)).setText("Invalid Password!");
+                    }
+                }
+            });
+        }
     }
 
 }
